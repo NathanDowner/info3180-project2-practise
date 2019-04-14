80f222e1ce08a44602941c966a265bc30ccf085c
Nathan Downer <nathan.downer@mymona.uwi.edu>
2019-04-14 23:25:28 +0000

    need to change follows to a db.Table

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Lab 5 Starter Code
-Starter code for Lab 5
+# Project 2
+Code for Projet 2 (Instagram clone)
--- app/models.py
+++ app/models.py
@@ -1,18 +1,43 @@
 from . import db
+from datetime import datetime
 
-
-class UserProfile(db.Model):
-    # You can use this to change the table name. The default convention is to use
-    # the class name. In this case a class name of UserProfile would create a
-    # user_profile (singular) table, but if we specify __tablename__ we can change it
-    # to `user_profiles` (plural) or some other name.
-    __tablename__ = 'user_profiles'
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    photo = db.Column(db.String(80))
+    caption = db.Column(db.String(80))
+    created_on = db.Column(db.DateTime, default=datetime.now)
+    
+class User(db.Model):
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80))
-    last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
-
+    password = db.Column(db.String(256), nullable=False)
+    firstname = db.Column(db.String(80), nullable=False)
+    lastname = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    location = db.Column(db.String(80), nullable=False)
+    biography = db.Column(db.String(50), nullable=False)
+    profile_photo = db.Column(db.String(80), nullable=False)
+    joined_on = db.Column(db.DateTime, default=datetime.now)
+    
+    #links
+    posts = db.relationship('Post', backref='user', lazy=True)
+    likes = db.relationship('Like', backref='user', lazy=True)
+    followers = db.relationship('Follower', backref=)
+    
+    def __init__(self, username, password, fname, lname, email, loc, bio, photo):
+        self.username = username
+        self.password = generate_password_hash(password, method='pbkdf2:sha256')
+        self.firstname = fname
+        self.lastname = lname
+        self.email = email
+        self.location = loc
+        self.biography = bio
+        self.photo = photo
+        
     def is_authenticated(self):
         return True
 
@@ -30,3 +55,34 @@ class UserProfile(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.username)
+        
+class Like(db.Model):
+    __tablename__ = 'likes'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    
+    def __init__(self, user_id, post_id):
+        self.user_id = user_id
+        self.post_id = post_id
+        
+    def __repr__(self):
+        return '<Like by {} on post {}>.format(self.user.username, self.post_id)'
+        
+follows = db.Table('follows',
+
+)
+    
+class Follow(db.Model):
+    __tablename__ = 'follows'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def __init__(self, user_id, follower_id):
+        self.user_id = user_id
+        self.follower_id = follower_id
+        
+    def __repr__(self):
+        return '<Follow {}>'.format(self.id)
+
